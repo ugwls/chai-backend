2717b3afa9ccee53d8ab739ed2f55df99956bd7d
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2025-06-01 01:15:43 +0530

    add user registration route and controller

---
+++ src/routers/user.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express"
+import { resgisterUser } from "../controllers/user.controller.js"
+
+const router = Router()
+
+router.route("/register").post(resgisterUser)
+
+export default router
